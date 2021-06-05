bbc217dd89d3c16d30785d0e8b379c7338ba93c3
Natalia Iwańska <262270@student.pwr.edu.pl>
2021-06-06 00:15:22 +0200

    add points for level 2

--- points.py
+++ points.py
@@ -58,10 +58,80 @@ def level_1():
             points_list.append(point)
 
     for x in range(18):
-        x = 112 + 36 * x
+        x = 113 + 36 * x
         y = 290
         point = arcade.Sprite("./images/point.png", SCALING)
         point.position = (x, y)
         points_list.append(point)
+
+    return points_list
+
+
+def level_2():
+    '''
+        Function inserts points for the map from level 2.
+        '''
+    points_list = arcade.SpriteList(use_spatial_hash=True)
+    for x in [80, 725]:
+        for y in range(17):
+            y = 50 + 30 * y
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for x in [150, 655]:
+        for y in range(13):
+            y = 110 + 30 * y
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for x in [222, 583]:
+        for y in range(7):
+            y = 185 + 35 * y
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for x in range(18):
+        x = 114 + 36 * x
+        y = 290
+        point = arcade.Sprite("./images/point.png", SCALING)
+        point.position = (x, y)
+        points_list.append(point)
+
+    for x in [365, 435]:
+        for y in [80, 500]:
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for y in [50, 530]:
+        for x in range(8):
+            x = 120 + 35 * x
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for y in [50, 530]:
+        for x in range(8):
+            x = 435 + 36 * x
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for y in [110, 470]:
+        for x in range(13):
+            x = 184 + 36 * x
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
+
+    for y in [185, 395]:
+        for x in range(9):
+            x = 260 + 36 * x
+            point = arcade.Sprite("./images/point.png", SCALING)
+            point.position = (x, y)
+            points_list.append(point)
 
     return points_list
